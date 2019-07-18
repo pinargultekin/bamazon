7d30a4cd14197c54b64ea1e16369b40f9525ee39
29bucuk <pinargultekin@yahoo.com>
2019-07-17 22:27:50 -0400

    Schema file updated

--- schema.sql
+++ schema.sql
@@ -12,6 +12,16 @@ stock_quantity int null,
 primary key (item_id)
 );
 
+create table departments(
+    department_id int not null auto_increment,
+    department_name VARCHAR(100),
+    over_head_cost int(10),
+    PRIMARY key(department_id)
+);
+
+alter table products
+add column product_sale int(10) not null after stock_quantity;
+
 insert into products(product_name, department_name, price, stock_quantity)
 values ("Air Circulator Fan", "Home Appliances", 49.98, 1);
 
@@ -43,3 +53,10 @@ insert into products(product_name, department_name, price, stock_quantity)
 values ("Novel: Fight Club", "Book", 24.65, 10);
 
 select * from products;
+
+select departments.department_id, departments.department_name, departments.over_head_cost, products.product_sale, (sum(products.product_sale)-departments.over_head_cost) 
+as total_profit
+from departments
+inner join products 
+on departments.department_name = products.department_name
+group by departments.department_id;
